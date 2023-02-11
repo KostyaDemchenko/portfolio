fcea15ae166a56875834aaf3df1abb25d8562d7b
KostyaDemchenko <kostya.doge@gmail.com>
2023-02-11 03:46:37 +0200

    menu fix

--- js/main.js
+++ js/main.js
@@ -322,7 +322,7 @@
 		fadeInTime  = 400,         // how slow/fast you want the button to show
 		fadeOutTime = 400,         // how slow/fast you want the button to hide
 		scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
-		goTopButton = $("#go-top")
+		goTopButton = $("#go-top");
 
 		// Show or hide the sticky footer button
 		$(window).on('scroll', function() {
